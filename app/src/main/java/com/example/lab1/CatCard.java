4e0e251704f8341a6d5d7c35d9dbe10f09229427
Yash Suresh <psyys5@nottingham.ac.uk>
2022-11-02 17:07:10 +0000

    first commit

---
+++ app/src/main/java/com/example/lab1/CatCard.java
@@ -0,0 +1,7 @@
+package com.example.lab1;
+
+public class CatCard
+{
+    public String catName;
+    public int resourceID;
+}
---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<CatCard> catList = new ArrayList<CatCard>();
+        String[] catNames = getResources().getStringArray(R.array.cat_names);
+
+        for(int i = 0; i <= 7; i++)
+        {
+            CatCard catcatCard = new CatCard();
+            catcatCard.resourceID = getResources().getIdentifier("@drawable/cat"+i,
+                    "drawable", getPackageName());
+
+            catcatCard.catName = catNames[i];
+
+
+        }
+
+
+    }
+}
